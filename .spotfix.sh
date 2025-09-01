3727f6dc469f346c6d8a16fbde7c7b85a095a401
echocobalt <windobats+github@proton.me>
2025-09-01 15:46:11 +0530

    FIXES TO PUSH BEFORE

--- .spotfix.sh
+++ .spotfix.sh
@@ -3,13 +3,22 @@
 SPOTIFY="com.spotify.Client"
 DIREC="$HOME/.var/app/$SPOTIFY"
 
-if pgrep -f "$APP" >/dev/null; then
-    pkill -f "$APP"
+if pgrep -f "$SPOTIFY" >/dev/null; then
+    pkill -f "$SPOTIFY"
 fi
 
 echo "Just Chillin' & Killin' cache/data..."
 rm -rf "$DIREC/cache" "$DIREC/data"
 
-echo "Guess who's back... back again... SPOTIFY's Back..."
-flatpak run "$SPOTIFY" &
+while true; do
+    read -p "Launch SPOTIFY? [Y/n] " consent
+    if [[ "$consent" == [Nn]* ]]; then
+        echo "HASTA LA VISTA... BABY!!!"
+        exit 0
+    elif [[ "$consent" == [Yy]* || -z "$consent" ]]; then
+        echo "Guess who's back... back again... SPOTIFY's Back..."
+        flatpak run "$SPOTIFY" &
+        break
+    fi
+done
 
